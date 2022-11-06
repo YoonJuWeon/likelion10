8e90d6d18838a4c255740206afffc20b444eb9b2
YoonJuWeon <78016830+YoonJuWeon@users.noreply.github.com>
2022-11-06 14:18:03 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# likelion10
+# Movie App 2020
+
+React JS Fundamentals Course 2020
---
+++ src/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+class Footer extends React.Component{
+    render(){
+        return (
+            <div className="app-footer">
+              MOVIE APP FOOTER
+            </div>
+        )
+    }
+}
+     
+export default Footer;
